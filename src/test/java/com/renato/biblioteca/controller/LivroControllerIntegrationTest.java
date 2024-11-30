99394b4e202434c9c759aaa49fd57e54c79614aa
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-11-30 19:59:37 -0300

    integration tests: livro controller

--- src/test/java/com/renato/biblioteca/controller/LivroControllerIntegrationTest.java
+++ src/test/java/com/renato/biblioteca/controller/LivroControllerIntegrationTest.java
@@ -4,11 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.renato.biblioteca.controller.dto.PostLivroDTO;
 
 import jakarta.transaction.Transactional;
 
@@ -25,7 +30,12 @@ public class LivroControllerIntegrationTest {
 	private ObjectMapper objectMapper;
 	
 	@Test
-	public void postLivro_sucesso() {
-		
+	public void postLivro_sucesso() throws JsonProcessingException, Exception {
+		//arrange
+		PostLivroDTO postLivroDTO = new PostLivroDTO("123", "123", "123", 123);
+		//act
+		mockMvc.perform(MockMvcRequestBuilders.post("/livros").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(postLivroDTO)))
+				.andExpect(MockMvcResultMatchers.status().isCreated());	
 	}
 }
